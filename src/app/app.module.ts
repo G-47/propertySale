eac59af042182fcd37c15dca9417b8e6545679ba
PriyashanJayasankha <priyashanshell@gmail.com>
2020-07-01 07:34:00 +0530

    home screen upper part created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/homePage/home/home.component';
 import { CardComponent } from './components/homePage/card/card.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CardComponent
+    CardComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
