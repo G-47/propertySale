462e5acab37208c60a26febab1d91910af5350b2
PriyashanJayasankha <priyashanshell@gmail.com>
2020-07-06 08:05:44 +0530

    profile added

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -6,9 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-  constructor() {}
+  lands = [];
 
-  x = [1, 1];
+  constructor() {
+    this.createLand(
+      'https://www.primelands.lk/resources/834/01.jpg',
+      'Land for sale in Weligama',
+      '350,000',
+      '40',
+      'Bare Lands',
+      'Weligama',
+      '15'
+    );
+    this.createLand(
+      'https://www.primelands.lk/resources/447/image1.jpg',
+      'Green Heaven Galle',
+      '1,000,000',
+      '35',
+      'Bare Land',
+      'Galle',
+      '18'
+    );
+
+    const x = this.lands.filter((item) => item.location.includes('Gal'));
+
+    console.log(x);
+  }
+
+  createLand(image, title, price, size, type, location, time) {
+    this.lands.push({
+      image,
+      title,
+      price,
+      size,
+      type,
+      location,
+      time,
+    });
+  }
 
   ngOnInit(): void {}
 }
