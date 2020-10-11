263b65cd93377750352874252b71f92719212330
Priyashan Jayasankha <priyashanshell@gmail.com>
2020-10-11 11:34:18 +0530

    added more 360 images

--- src/app/components/three-sixty-image/three-sixty-image.component.ts
+++ src/app/components/three-sixty-image/three-sixty-image.component.ts
@@ -1,29 +1,37 @@
-import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
-import {PanoViewer} from '@egjs/view360';
+import {
+  AfterViewInit,
+  Component,
+  OnInit,
+  ViewChild,
+  Input,
+} from '@angular/core';
+import { PanoViewer } from '@egjs/view360';
 
 @Component({
   selector: 'app-three-sixty-image',
   templateUrl: './three-sixty-image.component.html',
-  styleUrls: ['./three-sixty-image.component.scss']
+  styleUrls: ['./three-sixty-image.component.scss'],
 })
 export class ThreeSixtyImageComponent implements OnInit, AfterViewInit {
+  @ViewChild('360viewer', { static: false }) viewer;
 
-  @ViewChild('360viewer', {static: false}) viewer;
+  @Input() type: string;
 
   private panoViewer: PanoViewer;
 
   constructor() {}
 
   ngAfterViewInit(): void {
-    this.panoViewer = new PanoViewer(
-      this.viewer.nativeElement,
-      {
-        image: '/assets/images/360.jpeg'
-      }
-    );
+    if (this.type === 'land') {
+      this.panoViewer = new PanoViewer(this.viewer.nativeElement, {
+        image: `/assets/images/lands-360/${Date.now() % 3}.jpeg`,
+      });
+    } else if (this.type === 'house') {
+      this.panoViewer = new PanoViewer(this.viewer.nativeElement, {
+        image: `/assets/images/houses-360/${Date.now() % 3}.jpeg`,
+      });
+    }
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
